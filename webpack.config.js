772ffed039045e5c3fb09bcc5eba96f60965cd33
Martin Novak <ragnarecek@gmail.com>
2021-11-10 16:20:23 +1300

    refactor: webpack target commonjs2

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
+  target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'didauth.min.js',
-    library: 'DIDAuth',
-    libraryTarget: 'umd',
-    globalObject: 'this'
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
